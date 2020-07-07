00bc7ed9cd1bda35c7a4c77883ab28166878a023
eminakcil <eminakcil@outlook.com>
2020-07-07 21:26:19 +0300

    comment model & provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:orgeneral/model/Dealer.dart';
 import 'package:orgeneral/model/Product.dart';
+import 'package:orgeneral/model/Comment.dart';
 import 'package:orgeneral/services/db.dart';
 import 'package:provider/provider.dart';
 
@@ -29,8 +30,8 @@ class Example extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: StreamProvider<List<Dealer>>.value(
-        value: db.streamDealers(),
+      child: StreamProvider<List<Comment>>.value(
+        value: db.streamProductComments('AXvFV6xY7Y94PeDbFyHy', 'tof9O7PGJYTcZFcc4XYT'),
         child: ProductsList(),
       ),
     );
@@ -41,9 +42,9 @@ class ProductsList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // var products = Provider.of<List<Product>>(context);
-    var dealers = Provider.of<List<Dealer>>(context);
+    var dealers = Provider.of<List<Comment>>(context);
     return Column(
-      children: List.generate(dealers.length, (index) => Text('${dealers[index].name}')),
+      children: List.generate(dealers.length, (index) => Text('${dealers[index].content}')),
     );
   }
 }
--- lib/services/db.dart
+++ lib/services/db.dart
@@ -1,10 +1,12 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:orgeneral/model/Comment.dart';
 import 'package:orgeneral/model/Dealer.dart';
 import 'package:orgeneral/model/Product.dart';
 
 class DatabaseService {
   final Firestore _db = Firestore.instance;
 
+  //get product
   Stream<Product> streamProduct(String dealerId, String productId) {
     return _db
         .collection('dealers')
@@ -15,12 +17,14 @@ class DatabaseService {
         .map((snapshot) => Product.fromSnapshot(snapshot));
   }
 
+  //get products
   Stream<List<Product>> streamProducts(String dealerId) {
     var ref = _db.collection('dealers').document(dealerId).collection('products');
     return ref.snapshots().map((list) => 
         list.documents.map((snapshot) => Product.fromSnapshot(snapshot)).toList());
   }
 
+  //get dealer
   Stream<Dealer> streamDealer(String dealerId) {
     return _db
         .collection('dealers')
@@ -29,9 +33,27 @@ class DatabaseService {
         .map((snapshot) => Dealer.fromSnapshot(snapshot));
   }
 
+  //get dealers
   Stream<List<Dealer>> streamDealers() {
     var ref = _db.collection('dealers');
     return ref.snapshots().map((list) => 
         list.documents.map((snapshot) => Dealer.fromSnapshot(snapshot)).toList());
+  }
+
+  Stream<List<Comment>> streamDealerComments(String dealerId) {
+    var ref = _db.collection('dealers').document(dealerId).collection('comments');
+    return ref.snapshots().map((list) => 
+        list.documents.map((snapshot) => Comment.fromSnapshot(snapshot)).toList());
+  }
+
+  Stream<List<Comment>> streamProductComments(String dealerId, String productId) {
+    var ref = _db
+    .collection('dealers')
+    .document(dealerId)
+    .collection('products')
+    .document(productId)
+    .collection('comments');
+    return ref.snapshots().map((list) => 
+        list.documents.map((snapshot) => Comment.fromSnapshot(snapshot)).toList());
   }
 }
